0e9fa8a7016fb2d27585a1ff998a05fc86121a26
milk-maid <roqbell@gmail.com>
2023-04-09 01:06:13 +0100

    next n tailwindcss

---
+++ sections/About.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { typingText } from '../c'
+
+import styles from '../styles';
+import {  slideIn, staggerContainer, textVariant } from '../utils/motion';
+
+const About = () => (
+  <section>
+    About sectionhcd
+  </section>
+);
+
+export default About;
